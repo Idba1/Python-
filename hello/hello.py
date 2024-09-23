c52cca174c0d52406d6c4cd735861c1a0054c014
Idba1 <moniraislamself@gmail.com>
2024-09-24 04:53:52 +0600

    complete module 1

--- hello/hello.py
+++ hello/hello.py
@@ -1,1 +1,16 @@
 print("kopa Python")
+
+print(857854784 + 5899898)
+print("hello")
+print("Idba")
+# print(65%2)
+""" 
+    hello Idba
+"""
+print(7 + 8)
+
+# Single line comment ctrl + /
+""" 
+Multiline comment
+ctrl + alt + A
+"""
