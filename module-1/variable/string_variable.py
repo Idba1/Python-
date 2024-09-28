b717fa899d19e57e00ebb6177e1813045a81177d
Idba1 <moniraislamself@gmail.com>
2024-09-28 19:12:22 +0600

    type and f string module 1-7

--- module-1/variable/string_variable.py
+++ module-1/variable/string_variable.py
@@ -4,3 +4,6 @@ unil = "university"
 unifull = unif + " " + unim + " " + unil
 print(unifull)
 print("my name is Idba Islam.\ncurrently studying at", unifull)
+name = "Idba Islam"
+about_me = f"my name is {name}. currently i'm studying at {unifull}......"
+print(about_me)
---
+++ module-1/variable/type.py
@@ -0,0 +1,9 @@
+num = 17
+num_Type = type(num)
+print(num_Type)
+name = "owasfia"
+is_good = True
+numf=3.76
+print(type(name))
+print(type(is_good))
+print(type(numf))
