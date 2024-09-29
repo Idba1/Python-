ec115eb2a95aaced3e830720e29d182f7c158bcb
Idba1 <moniraislamself@gmail.com>
2024-09-29 11:07:44 +0600

    nested condition with if, elif, else

---
+++ module-2/nestedCondition.py
@@ -0,0 +1,25 @@
+cgpa = "2.00"
+if cgpa >= "3.50":
+    if cgpa == "4.00":
+        print("you will get gold madal")
+        print("congrets")
+    else:
+        print("your result is good. all the best!")
+elif cgpa >= "3.00":
+    print("its okay. but you need to try..!")
+else:
+    print("Bhai pora likha kor..!!!")
+
+
+# MODULE CODE
+salary = 5000
+has_flat = True
+
+if salary > 35000:
+    if has_flat == True:
+        print("Ok tomar jonno manus ase")
+    else:
+        print("tor kopale kichu nai")
+        print("But dont worry. enjoy your life")
+else:
+    print("Bepar na. sobar ekdin somoy asbe")
