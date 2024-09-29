ef3a8f29006dd652c9242a3e64cbe3eae0583773
Idba1 <moniraislamself@gmail.com>
2024-09-29 10:42:48 +0600

    conditionals and comparison

---
+++ module-2/conditionals.py
@@ -0,0 +1,21 @@
+examMark = input("enter your exam mark: ")
+iExamMark = int(examMark)
+if iExamMark >= 40:
+    print("you passed")
+elif iExamMark < 40:
+    print("Failed")
+else:
+    print("enter a valid number")
+
+
+
+
+biryani_price = 230
+kabab_price = 45
+
+if biryani_price < 150:
+    print("Sultan e khamu fokir homu")
+elif kabab_price < 50:
+    print("kabab and naan, aram kore khan")
+else:
+    print("butter bon khamu cha diya")
