1ca5484213c6e30258d64980a85acc1f93304d40
Idba1 <moniraislamself@gmail.com>
2024-09-29 16:22:54 +0600

    while example

---
+++ module-2/whileexample.py
@@ -0,0 +1,32 @@
+number = 0
+while number < 10:
+    print(number)
+    number = number + 1
+
+# ODD NUMBER
+number = 1
+while number < 10:
+    print("odd number", number)
+    number = number + 2
+
+
+# EVEN NUMBER
+number = 0
+while number < 10:
+    number = number + 2
+    print("Even number", number)
+
+
+# MODULE CODE
+number = 1
+while number < 5:
+    # print(number)
+    number = number + 1
+
+total = 0
+num = 1
+while num <= 5:
+    total = total + num
+    num = num + 1
+    print(total)
+print("outside of while")
