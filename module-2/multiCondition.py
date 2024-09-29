57e20dfba914b47c8f9b784eb5e3f4470d9206ef
Idba1 <moniraislamself@gmail.com>
2024-09-29 11:00:11 +0600

    module code

--- module-2/multiCondition.py
+++ module-2/multiCondition.py
@@ -21,3 +21,30 @@ if device == "laptop" and device2 == "computer":
     print("you can start programming")
 else:
     print("you need to buy laptop or desktop computer")
+
+
+# MODULE CODE
+
+exam = "Private Job"
+passed = False
+second = "Bank"
+
+if exam == "BCS" and passed == True:
+    print("WOW tor biye to pakka")
+    print("Congrats Mamma")
+else:
+    print("to biye hobe na")
+
+
+if exam == "BCS" or second == "Bank":
+    print("tor life to set hoye gese")
+else:
+    print("Tui ki ajibon bekar thakbi")
+
+# if exam == 'BCS':
+#     print('biyer bazare dami patro patri')
+
+# if passed == True:
+#     print('congrats')
+# else:
+#     print('try next time again')
