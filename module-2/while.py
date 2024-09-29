e61c0c5ebd8bc0514e07ca033c067640e4a6546e
Idba1 <moniraislamself@gmail.com>
2024-09-29 11:57:49 +0600

    while loop

---
+++ module-2/while.py
@@ -0,0 +1,24 @@
+sum = 0
+number = 1
+cNumber = input()
+icNumber = int(cNumber)
+while number <= icNumber:
+    sum = sum + number
+    print(number)
+    number = number + 1
+print(sum)
+
+
+# MODULE CODE
+num = 1
+while num <= 10:
+    print(num)
+    num = num + 1
+
+
+sum = 0
+num = 1
+while num <= 10:
+    sum = sum + num
+    num = num + 1
+print(sum)
