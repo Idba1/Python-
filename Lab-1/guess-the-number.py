277c10da270951f44ef56080cf3bd91b6290bc79
Idba1 <moniraislamself@gmail.com>
2024-11-22 22:53:04 +0600

    guess the number

---
+++ Lab-1/guess-the-number.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+i = input("Guess a number ")
+i = input("Multiply 2 with your number ")
+i = input("Add 8 with the result ")
+i = input("divide the whole result with 2 ")
+i = input("Subtract the guessed number with the result")
+print("your guessed number is 4\n")
