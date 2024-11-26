d9b3476f5dc248a22e10e7d57e30b94d51f3b5c9
Idba1 <moniraislamself@gmail.com>
2024-11-26 23:30:12 +0600

    file append and read

--- Module-3/write.py
+++ Module-3/write.py
@@ -1,2 +1,11 @@
-with open("message.txt", "w") as fileWrite:
-    fileWrite.write("Hello from python")
+# with open("message.txt", "w") as fileWrite:
+#     fileWrite.write("second test")
+
+
+with open("append.txt", "a") as fileWrite:  # append korbe
+    fileWrite.write("second test\n")
+
+# read
+with open("append.txt", "r") as fileRead:  # read file
+    text = fileRead.read()
+    print(text)
