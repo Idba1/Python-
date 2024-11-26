c07b2ad13da2dc28b3bce332e6f4df3b5fc261a5
Idba1 <moniraislamself@gmail.com>
2024-11-26 17:29:55 +0600

    modules.py

--- Module-3/function.py
+++ Module-3/function.py
@@ -1,5 +1,16 @@
 def double_it(num):
-    result = num*2
-    print(result)
+    result = num * 2
+    # print(result)
+    return result
+
 
 double_it(11)
+
+
+def sum(num, num2):
+    result = num + num2
+    # print(result)
+    return result
+
+
+sum(11, 22)
---
+++ Module-3/modules.py
@@ -0,0 +1,21 @@
+# WAY__1
+# import function
+
+# total = function.sum(2, 3)
+# print(total)
+
+# WAY__2
+# from function import sum, double_it
+
+# total = sum(2, 4)
+# print(total)
+
+# mult = double_it(4)
+# print(mult)
+
+
+# WAY__3
+from function import *
+
+mult2 = double_it(2)
+print(mult2)
