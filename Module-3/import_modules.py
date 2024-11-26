965deadf11026a91a9e77f8d29a3413ce0a6c8e7
Idba1 <moniraislamself@gmail.com>
2024-11-26 17:44:37 +0600

    Build In Modules And Build In Functions

---
+++ Module-3/import_modules.py
@@ -0,0 +1,18 @@
+import math
+import random
+import datetime as dt
+
+result = math.ceil(2.323)
+print(result)
+
+ran = random.random()
+print(ran)
+ran2 = random.randint(12, 14)
+print(ran2)
+
+winner = random.choice([1, 2, 3, 4, 4, 55, 23, 23, 324])
+print(winner)
+
+
+current_time = dt.time()
+print(current_time)
