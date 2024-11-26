fa6264df327751a8d1aa240eba886324b9abb9c3
Idba1 <moniraislamself@gmail.com>
2024-11-26 23:55:22 +0600

    list, insert, pop , push, count, sort,remove

--- Module-4/list.py
+++ Module-4/list.py
@@ -8,3 +8,20 @@ print(numbers[3:5])  # slice. for this instruction it will be taken 3rd and 4th 
 print(numbers[3:-5])
 print(numbers[0:9:2])
 print(numbers[4:0:-1])  # reverse
+print(numbers[:])  # print full array
+print(numbers[::-1])  # full array reverse
+
+numbers.append(334)
+print(numbers[:])
+numbers.pop()
+print(numbers[:])
+numbers.sort()  # sort
+print(numbers[:])
+
+numbers.remove(4)
+print(numbers[:])
+
+numbers.insert(0, 122)
+print(numbers[:])
+count = numbers.count(23)
+print(count)
