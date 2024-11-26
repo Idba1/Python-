bb4d8b3aabb13b512f9b97b85a34613f90337cf7
Idba1 <moniraislamself@gmail.com>
2024-11-26 23:48:56 +0600

    list, list methods and slice

---
+++ Module-4/list.py
@@ -0,0 +1,10 @@
+numbers = [12, 23, 4, 534, 6345, 23, 234, 32, 354]
+
+print(numbers[8])  # index 0,1,2,3,4,5,6,7
+print(numbers[-1])  # index -1,-2,-3,-4,-5,-6,-7,-8,-9
+
+# list [numbers[start:end:step]]
+print(numbers[3:5])  # slice. for this instruction it will be taken 3rd and 4th value.
+print(numbers[3:-5])
+print(numbers[0:9:2])
+print(numbers[4:0:-1])  # reverse
