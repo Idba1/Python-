37a73667929a9aff45d7632c2988297ba11a8fb8
Idba1 <moniraislamself@gmail.com>
2024-11-27 00:46:06 +0600

    Dictionary and Dictionary Methods

---
+++ Module-4/dictionary.py
@@ -0,0 +1,13 @@
+marks = {"phy": 14, "che": 98, "bio": 34}
+print(marks)
+
+# add one item
+marks["math"] = 23
+print(marks)
+
+# get all keys
+print(marks.keys())
+# get all values
+print(marks.values())
+# get all items
+print(marks.items())
