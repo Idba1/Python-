64d05656c61407f088d5ad2e2b7dff8b609510e4
Idba1 <moniraislamself@gmail.com>
2024-11-27 22:51:59 +0600

    basic lambda

---
+++ Module-4/lambda.py
@@ -0,0 +1,13 @@
+def square(x):
+    return x * x
+
+
+print(square(2))
+print("end\n")
+
+# this function in one line
+square = lambda x: x * x
+print(square(2))
+
+add = lambda x, y: x + y
+print(add(1, 3))
