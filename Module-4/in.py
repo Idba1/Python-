b8f846a25e989fd947b36021c052619a57d9631b
Idba1 <moniraislamself@gmail.com>
2024-11-28 01:04:31 +0600

    Check if values exist in the list using 'in' and 'not in' operators

---
+++ Module-4/in.py
@@ -0,0 +1,13 @@
+numbers = [12, 23, 4, 534, 63, 63, 23, 234, 35, 354]
+if 43 in numbers:
+    print("founded!")
+if 43 not in numbers:
+    print("not founded!")
+
+if 354 in numbers:
+    print("founded!")
+if 354 not in numbers:
+    print("not founded!")
+
+for num in numbers:
+    print(num)
