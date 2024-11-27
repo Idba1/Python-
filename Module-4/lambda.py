d60b431fadb7e9d2d1c2a4ec435d3ab5b83b7c5b
Idba1 <moniraislamself@gmail.com>
2024-11-27 22:59:27 +0600

    double a variable using lambda

--- Module-4/lambda.py
+++ Module-4/lambda.py
@@ -11,3 +11,14 @@ print(square(2))
 
 add = lambda x, y: x + y
 print(add(1, 3))
+
+numbers = [12, 23, 4, 534, 63, 63, 23, 234, 35, 354]
+doubled_it = lambda x: x * 2
+doubleList = []
+print(doubled_it(8))
+for num in numbers:
+    double = doubled_it(num)
+    print(double)
+    doubleList.append(double)
+print(numbers)
+print(doubleList)
