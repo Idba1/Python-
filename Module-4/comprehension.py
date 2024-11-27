c2f9dc492330a1f1f863be209eb6d4d5bf81a76e
Idba1 <moniraislamself@gmail.com>
2024-11-27 23:48:13 +0600

    One line for loop and condition using list comprehension

--- Module-4/comprehension.py
+++ Module-4/comprehension.py
@@ -15,3 +15,15 @@ odd_num2 = [num for num in numbers if num % 2 == 1 and num % 5 == 0]
 print(odd_num2)
 even_num = [num for num in numbers if num % 2 == 0]
 print(even_num)
+
+
+names = ["sakib", "rakib", "asib", "karib"]
+ages = [23, 45, 34, 30]
+pairs = [(name, age) for name in names for age in ages if age < 30]
+print(pairs)
+
+for name in names:
+    print(name)
+    for age in ages:
+        if age < 30:
+            print(name, age)
