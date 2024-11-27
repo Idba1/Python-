8161c98af581503f4f60c9dec0c9be277da29c72
Idba1 <moniraislamself@gmail.com>
2024-11-27 23:12:46 +0600

    lamda with filter

--- Module-4/lambda.py
+++ Module-4/lambda.py
@@ -33,3 +33,17 @@ print(list(doubled_it2))
 # in shortcut
 doubled_num = map(lambda x: x * 2, numbers)
 print("output for shortcut-->", list(doubled_num))
+
+
+# filter
+numbers = [0, -12, -45, 100, 12, 23, 4, 534, 63, 63, 23, 234, 35, 354]
+bigger_numbers = filter(lambda num: num >= 100, numbers)
+print(list(bigger_numbers))
+lower_numbers = filter(lambda num: num < 100 and num >= 0, numbers)
+print(list(lower_numbers))
+negative_numbers = filter(lambda num: num < 0, numbers)
+print(list(negative_numbers))
+even_numbers = filter(lambda num: num % 2 == 0 and num > 0, numbers)
+print(list(even_numbers))
+odd_numbers = filter(lambda num: num % 2 == 1 and num > 0, numbers)
+print(list(odd_numbers))
