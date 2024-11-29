bc225b959a73731710e077c4fd0ce03fbb3f76c4
Idba1 <moniraislamself@gmail.com>
2024-11-30 03:16:14 +0600

    class in python

---
+++ Module-5/class-phone.py
@@ -0,0 +1,12 @@
+class phone:
+    brand = "samsung"
+    color = "black"
+    price = 32000
+
+
+my_phone = phone()
+print(my_phone)
+print(my_phone.brand)
+
+dad_phone = phone()
+print(dad_phone.price)
